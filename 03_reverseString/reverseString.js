45df9bf03b49023dde9158ce8856560502ff615c
Simonas Kulakauskas <50331279+simonas-kulakauskas@users.noreply.github.com>
2024-06-17 19:57:22 +0100

    Changed my solution (03) to given solution. Runs about twice as fast.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,10 +1,5 @@
 const reverseString = function (string) {
-    let newString = "";
-
-    for (i = (string.length - 1); i >= 0; i--) {
-        newString += string[i]
-    }
-    return (newString)
+    return string.split("").reverse().join("");
 };
 
 // Do not edit below this line

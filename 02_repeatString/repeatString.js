aa84b0e93ab8132890214ce7ec1390ce817c9383
Simonas Kulakauskas <50331279+simonas-kulakauskas@users.noreply.github.com>
2024-06-17 19:41:46 +0100

    Completed tasks 01, 02. - Tests ran and working

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function (chosenWord, repeatQuantity) {
+    let joinedString = "";
+    if (repeatQuantity < 0) {
+        return "ERROR";
+    }
+    else {
+        for (i = 0; i < repeatQuantity; i++) {
+            joinedString += chosenWord;
+        }
+        return joinedString;
+    }
 };
 
 // Do not edit below this line

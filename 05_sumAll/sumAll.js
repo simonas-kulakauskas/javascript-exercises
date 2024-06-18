975bd6dca94d0ae05752edee058bedf5356f6010
Simonas Kulakauskas <50331279+simonas-kulakauskas@users.noreply.github.com>
2024-06-19 00:35:36 +0100

    Completed 05. - Mostly the same as solution. 	- checked negative/non-integer type myself rather than using a method.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,31 @@
-const sumAll = function() {
+const sumAll = function (number1, number2) {
+    if ((number1 < 0) || (number2 < 0)) { // Filter out negative numbers.
+        return "ERROR";
+    } else if ((typeof number1 !== 'number')
+        || (typeof number2 !== 'number')) { // Filter out non-numbers.
+        return "ERROR";
+    }
 
+    // Ensure smallest/largest numbers are in correct position
+    let min, max = 0;
+    if (number1 > number2) {
+        min = number2;
+        max = number1;
+    }
+    else if (number1 < number2) {
+        min = number1;
+        max = number2;
+    }
+
+    // Loop through from min to max, adding together.
+    let total = 0;
+    for (let i = min; i <= max; i++) {
+        total += i;
+    }
+    return total;
 };
+
+//console.log(sumAll(1, 4))
 
 // Do not edit below this line
 module.exports = sumAll;

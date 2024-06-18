2800ab8b2694f1b58db0f0df9f1958d1a2568376
Simonas Kulakauskas <50331279+simonas-kulakauskas@users.noreply.github.com>
2024-06-19 00:47:54 +0100

    Completed 06 - Perfect example, just as solution. - Text-based truth table for the win!

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,26 @@
-const leapYears = function() {
-
+const leapYears = function (userYear) {
+    if (userYear % 4 === 0 && ((userYear % 100 !== 0) || (userYear % 400 === 0))) {
+        return true;
+    } else { return false; }
 };
-
+console.log(`Is 2000 a leap year? : ${leapYears(2000)}`);
+console.log(`Is 1985 a leap year? : ${leapYears(1985)}`)
 // Do not edit below this line
 module.exports = leapYears;
+
+/*
+Logic Flowchart:
+    Year divisible 4?
+    True:
+        Year divisible by 100?
+        True: 
+            Year divisible by 400?
+            True:
+                LEAP YEAR!
+            False:
+                NOT LEAP YEAR!
+        False:
+        NOT LEAP YEAR!
+    False:
+    NOT LEAP YEAR!
+*/

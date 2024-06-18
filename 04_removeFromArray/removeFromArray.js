eec6cad48128ed41ebccccc27ea1d61b3428c427
Simonas Kulakauskas <50331279+simonas-kulakauskas@users.noreply.github.com>
2024-06-18 15:21:27 +0100

    So for, able to find index of one element - need to work on allowing you to repeat this for all args

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -2,23 +2,32 @@ const removeFromArray = function () {
 
     // Setting up the variables and returning if none were given.
     let array = arguments[0];
-    let args;
 
-    const checkIfAnyArgs = () => { // Creates an array instance from arguments, cuts off first result (array to be deleted from)
-        let args = Array.from(arguments).slice(1);
-        if (args.length <= 0) {
-            console.log("No arguments were sent!");
-            return null;
-        }
-        else { return args; }
-    }
+    //let args;
+    // const checkIfAnyArgs = () => { // Creates an array instance from arguments, cuts off first result (array to be deleted from)
+    //     let args = Array.from(arguments).slice(1);
+    //     if (args.length <= 0) {
+    //         console.log("No arguments were sent!");
+    //         return null;
+    //     }
+    //     else { console.log("Args were found!"); return args = args; }
+    // }
 
 
-    if (checkIfAnyArgs() === null) { // Making sure that one or more arguments was sent, send it back if not, continue if they were.
+    // if (checkIfAnyArgs() === null) { // Making sure that one or more arguments was sent, send it back if not, continue if they were.
+    //     return (array);
+    // } else if (checkIfAnyArgs().length > 0) {
+    //     args = checkIfAnyArgs();
+    // }
+    let args = Array.from(arguments).slice(1);
+    if (args.length <= 0) {
+        console.log("No arguments sent!, Returning array!")
         return (array);
-    } else if (checkIfAnyArgs().length > 0) {
-        args = checkIfAnyArgs();
     }
+
+
+
+
 
     // Setting up Mechanism to actually do the main deleting part
 
@@ -29,11 +38,12 @@ const removeFromArray = function () {
 
     const findIndexOfElementInArray = (array, element) => { // To find index of all element instances to be removed in array.
         let elementIndex = [];
-        for (i = 0; i < array.length; i++) {
+        for (let i = 0; i < array.length; i++) {
             if (array[i] === element) {
                 elementIndex.push(i);
             }
         }
+
         if (elementIndex.length <= 0) {
             console.log(`No elements of array '${array.toString()}' were found to match ${element}! Returning Null...`);
             return null;
@@ -42,13 +52,20 @@ const removeFromArray = function () {
         }
     }
 
-    console.log(findIndexOfElementInArray([1, 2, 3, 4], 1))
+    const findAllRelevantIndex = (array, args) => { // *** Should be able to find all relevant indexs (using findIndexOfElementInArray) and tabulate them all to pass later.
+
+    }
+
+
+    console.log(findIndexOfElementInArray(array, args[0]));
+
+
 
 
 
 };
 
-console.log(removeFromArray([1, 2, 3, 4], 1))
+console.log(removeFromArray([1, 2, 3, 4, 1], 1))
 
 
 

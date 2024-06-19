526d546791d55e5752b77743f9bef7a497d3eef8
Simonas Kulakauskas <50331279+simonas-kulakauskas@users.noreply.github.com>
2024-06-19 02:46:36 +0100

    Completed 10 - My solution was a bit different to theirs but still works perfectly fine without being 'cheatsy'.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,23 @@
-const fibonacci = function() {
+const fibonacci = function (inputNumber) {
+    let fib1 = 0;
+    let fib2 = 1;
+    let fibonacciNumber = 1;
 
+    inputNumber = Math.round(inputNumber);
+
+
+    if (inputNumber == 0) { return 0; } // For loop won't function with 0, so return 0 if it was inputted.
+    else if (inputNumber < 0) { return "OOPS"; } // Don't allow negative numbers because they don't work.
+
+    for (i = 1; i < inputNumber; i++) {
+        fibonacciNumber = fib1 + fib2
+        fib1 = fib2
+        fib2 = fibonacciNumber
+    }
+    return fibonacciNumber;
 };
+
+
 
 // Do not edit below this line
 module.exports = fibonacci;

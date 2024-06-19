06f7ccf0b1d150753dc04a547fc65bf4fdba4c8b
Simonas Kulakauskas <50331279+simonas-kulakauskas@users.noreply.github.com>
2024-06-19 01:43:20 +0100

    Completed 'sum' function.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -7,13 +7,17 @@ const subtract = function (num1, num2) {
 };
 
 const sum = function () {
-
+  let total = 0;
+  for (i = 0; i < arguments[0].length; i++) {
+    total += arguments[0][i]
+  }
+  return total;
 };
 
 const multiply = function () {
   let total = arguments[0][0];
 
-  for (let i = 1; i < arguments[0].length; i++) {
+  for (i = 1; i < arguments[0].length; i++) {
     total = total * arguments[0][i];
   }
 

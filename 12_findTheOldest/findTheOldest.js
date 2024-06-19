9c8b65fef0a681bf822c5a9e8a041224a8a4987d
Simonas Kulakauskas <50331279+simonas-kulakauskas@users.noreply.github.com>
2024-06-19 03:32:34 +0100

    Finished 12!!! Finished these exercises!!!! - Not as efficent or simplistic as solution (yet again) but it still works :)))

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,45 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
 
+    const currentDate = new Date();
+    const currentYear = currentDate.getFullYear();
+
+    const fillUndefinedDeath = (person) => {
+        person.yearOfDeath = currentYear;
+        return person;
+    }
+
+    const workOutAge = (person) => {
+        return (person['yearOfDeath'] - person['yearOfBirth']);
+    }
+    const setFirstOldest = () => {
+        if (typeof people[0]['yearOfDeath'] == 'undefined') {
+            people[0] = fillUndefinedDeath(people[0]);
+        }
+        return people[0];
+    }
+
+    const workOutOldest = () => {
+
+        let oldestPerson = setFirstOldest();
+        let oldestAge = workOutAge(oldestPerson);
+
+        for (i = 1; i < people.length; i++) {
+            let newAge = workOutAge(people[i]);
+            let newPerson = people[i];
+
+            if (oldestAge < newAge) {
+                oldestAge = newAge;
+                oldestPerson = newPerson;
+            }
+        }
+        return oldestPerson;
+    }
+
+    return workOutOldest();
 };
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
+
+

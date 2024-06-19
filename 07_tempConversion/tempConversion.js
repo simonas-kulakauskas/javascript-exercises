3b4a079a6c9b598b92902fb58ce4188fb0f0621a
Simonas Kulakauskas <50331279+simonas-kulakauskas@users.noreply.github.com>
2024-06-19 01:16:45 +0100

    Completed 07 - Initally started with variables established and checking if numbers had a decimal before chosing whether to round to one decimal place or one whole number. - Realised '.toFixed()' method returns a string rather than a number (because reasons). - Changed to a system that makes the number whole before rounding it, then turns it back into a decimal before returning. 	- Simple, yet beautiful and the same as solution.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,26 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (temp) {
+  // let convertedC = ((temp - 32) * (5 / 9));
+
+  // if (convertedC % 1 !== 0) { return convertedC.toFixed(1); }
+  // else { return convertedC.toFixed(0); }
+  return (Math.round(((temp - 32) * (5 / 9)) * 10) / 10);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (temp) {
+  // let convertedF = ((temp * (9 / 5)) + 32);
+  // if (convertedF % 1 !== 0) { return convertedF.toFixed(1); }
+  // else { return convertedF.toFixed(0); }
+
+  return (Math.round(((temp * (9 / 5)) + 32) * 10) / 10);
 };
+
+console.log(convertToFahrenheit(-10));
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
   convertToFahrenheit
 };
+
+// .toFixed(1)
+

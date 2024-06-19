7ecdfae53f5c2bc68638b58a6a66a604e21b6fb1
Simonas Kulakauskas <50331279+simonas-kulakauskas@users.noreply.github.com>
2024-06-19 02:18:08 +0100

    Completed task 09. Needed to use some stack overflow magic to remove punctuation. After checking solution, will need to read up on filtering strings better :)

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,18 @@
-const palindromes = function () {
+const palindromes = function (sentence) {
 
+    sentence = (sentence
+        .replace(/[^\w\s\']|_/g, "") // Replacement magic, no idea how it works but it does :)
+        .replaceAll(" ", "") // To compare string without spaces
+        .toLowerCase() // To compare without capitalization
+    );
+
+    let reversedSentence = sentence.split("").reverse().join(""); // Reverse string to have something to compare, split it into array since reverse method needs it, reverse it then rejoin into string.
+
+    if (sentence === reversedSentence) { return true; }
+    else { return false };
 };
+
 
 // Do not edit below this line
 module.exports = palindromes;
+

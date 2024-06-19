36693a5ab48ee55e0fbf2c11d7dd8f04da9cbe6c
Simonas Kulakauskas <50331279+simonas-kulakauskas@users.noreply.github.com>
2024-06-19 01:19:22 +0100

    Removed old code in comments. If needed or want to look, check previous commit to 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,16 +1,8 @@
 const convertToCelsius = function (temp) {
-  // let convertedC = ((temp - 32) * (5 / 9));
-
-  // if (convertedC % 1 !== 0) { return convertedC.toFixed(1); }
-  // else { return convertedC.toFixed(0); }
   return (Math.round(((temp - 32) * (5 / 9)) * 10) / 10);
 };
 
 const convertToFahrenheit = function (temp) {
-  // let convertedF = ((temp * (9 / 5)) + 32);
-  // if (convertedF % 1 !== 0) { return convertedF.toFixed(1); }
-  // else { return convertedF.toFixed(0); }
-
   return (Math.round(((temp * (9 / 5)) + 32) * 10) / 10);
 };
 
@@ -22,5 +14,5 @@ module.exports = {
   convertToFahrenheit
 };
 
-// .toFixed(1)
+
 
